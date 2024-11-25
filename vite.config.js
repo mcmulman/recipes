ec982166203a3e68ee6c74f141dc39380d9dda85
M.Uhlmann (MMS) <mathias.uhlmann@telekom.de>
2024-11-25 22:41:21 +0100

    Use relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: './docs',
+    relative: true, // necessary for GitHub Pages
     emptyOutDir: true, // also necessary
   }
 });
