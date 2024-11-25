fb84c7df6bda30c12271c92330b928652364a7a1
M.Uhlmann (MMS) <mathias.uhlmann@telekom.de>
2024-11-25 22:44:20 +0100

    Use relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // necessary for GitHub Pages
   build: {
     outDir: './docs',
     relative: true, // necessary for GitHub Pages
