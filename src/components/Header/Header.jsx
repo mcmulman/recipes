2a3e9a9559522580a5084db0d1e255e29d80737a
M.Uhlmann (MMS) <mathias.uhlmann@telekom.de>
2024-11-26 14:16:31 +0100

    Add vegan image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.css';
 import recipeImg from '../../assets/recipes.svg';
+import veganImg from '../../assets/vegan.svg';
 
 function Header() {
   return (
     <header>
-      <h1><img src={recipeImg}  /> Rezepte</h1>
+      <h1><img src={recipeImg} id={'recipeImg'} /> Rezepte <img style={{position: 'relative', top: '-12px', left: '-6px'}} width={'32'} src={veganImg}/></h1>
     </header>
   );
 }
